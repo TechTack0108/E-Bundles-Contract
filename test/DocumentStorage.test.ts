53c69a35270ca2b715bdbba69486fa3aa19adc9f
ctb0k33 <ctb0k33@gmail.com>
2024-04-23 00:41:51 +0700

    [FEAT] build multisig wallet for party confirmation

--- test/DocumentStorage.test.ts
+++ test/DocumentStorage.test.ts
@@ -7,33 +7,15 @@ describe("Document Storage", () => {
     let documentStorage: DocumentStorage;
     let deployer: string;
 
-    beforeEach(async () => {
-        // Deploy the contract
-        await deployments.fixture(["all"]);
-
-        deployer = (await getNamedAccounts()).deployer;
-
-        const documentStorageContract = await deployments.get("DocumentStorage");
-        documentStorage = await ethers.getContractAt(documentStorageContract.abi,
-            documentStorageContract.address,
-            await ethers.getSigner(deployer)) as BaseContract as DocumentStorage;
-    })
-
-    describe("Add a document", () => {
-        it("should add a document", async () => {
-            const name = "test";
-            const hash = "0x123456";
-            const timeStamp = 1112123;
-
-            const transaction = await documentStorage.addDocument(name, hash, timeStamp, relativeParty, version);
-            await transaction.wait(1);
-
-            const document = await documentStorage.getDocument(hash);
-            assert.equal(document.name, name);
-            assert.equal(document.timestamp.toString(), timeStamp.toString());
-            assert.equal(document.relativeParty.length, relativeParty.length);
-            assert.equal(document.version, version);
-        }
-        )
-    })
+//     beforeEach(async () => {
+//         // Deploy the contract
+//         await deployments.fixture(["all"]);
+//
+//         deployer = (await getNamedAccounts()).deployer;
+//
+//         const documentStorageContract = await deployments.get("DocumentStorage");
+//         documentStorage = await ethers.getContractAt(documentStorageContract.abi,
+//             documentStorageContract.address,
+//             await ethers.getSigner(deployer)) as BaseContract as DocumentStorage;
+//     })
 })
